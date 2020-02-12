ea8d48d8afc5c5f164f9533cc625153039371792
Sergio Armando Rodriguez Ramirez <sergioardz79@gmail.com>
2020-02-11 21:35:33 -0600

    Pseudocode to download and start Tomcat

--- script2.sh
+++ script2.sh
@@ -1,1 +1,27 @@
 #!/bin/bash
+
+# PSEUDO CODE To download, unpack and start Tomcat within 'Artemio's ssh connection server
+
+# 1 Download Tomcat
+# Use wget command to download file
+wget https://www-eu.apache.org/dist/tomcat/tomcat-9/v9.0.30/src/apache-tomcat-9.0.30.tar.gz
+
+# 2 Unpack downloaded file
+tar xvzf apache-tomcat-9.0.30-src.tar.gz
+
+# 3 Set JAVA_HOME using /opt/java/jdk1.8.0_241
+export JAVA_HOME='/opt/java/jdk1.8.0_241'
+
+# 4 Update $PATH
+#export PATH=$JAVA_HOME/bin:$PATH
+
+# Switch to tomcat folder
+cd apache-tomcat-9.0.30/bin/
+
+# Check permissions
+#chmod +x *.sh
+
+# 5 Run Tomcat and test localhost
+./startup.sh
+
+# Finally open browser an check localhost to test Tomcat
